85d77bf990c294f37f586a5ecb9d483c9ac81748
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2021-08-25 20:39:07 -0300

    agregando suma de 2 nros

---
+++ Clase-1-2021-1c.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Clase_1_2021_1c.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //comentar: ctrl + K, C
+            //descomentar: ctrl + K, U
+            Console.WriteLine("Ingrese un nro:");
+            string strNum1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese otro nro:");
+            string strNum2 = Console.ReadLine();
+
+            int num1 = int.Parse(strNum1);
+            int num2 = int.Parse(strNum2);
+
+            Console.WriteLine("La suma es: " + (num1 + num2));
+        }
+    }
+}
