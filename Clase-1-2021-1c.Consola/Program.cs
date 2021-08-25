8ea89424ba19b226949691f89634a6a7cac6b780
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2021-08-25 20:19:11 -0300

    estructura inicial

---
+++ Clase-1-2021-1c.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_1_2021_1c.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
