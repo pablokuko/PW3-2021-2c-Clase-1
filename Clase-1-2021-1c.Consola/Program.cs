7748ed73c36b73a903a2e907e9feb4d6af4ffe1e
Gaston Perez <gastonperez-97@hotmail.com>
2021-08-31 18:11:12 -0300

    added bmi exercise

--- Clase-1-2021-1c.Consola/Program.cs
+++ Clase-1-2021-1c.Consola/Program.cs
@@ -1,24 +1,43 @@
 ﻿using PW3_2021_2c_Clase_1.Logica;
 using System;
 
-namespace Clase_1_2021_1c.Consola
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            EjercicioBola8Magica();
-            //EjericicioCalculadora();
-        }
+namespace Clase_1_2021_1c.Consola {
+    class Program {
+        static void Main(string[] args) {
+			EjercicioIMC();
+			//EjercicioBola8Magica();
+			//EjercicioCalculadora();
+		}
 
-        private static void EjercicioBola8Magica()
-        {
+		private static void EjercicioIMC() {
+			double height = -1;
+			double weight = -1;
+			double bmi;
+
+			string category;
+			string subCategory;
+
+			while (height <= 0) {
+				height = BodyMassIndex.askForHeight();
+			}
+
+			while (weight <= 0) {
+				weight = BodyMassIndex.askForWeight();
+			}
+
+			bmi = BodyMassIndex.calcBMI(height, weight);
+			category = BodyMassIndex.getCategoryConsidering(bmi);
+			subCategory = BodyMassIndex.getSubcategoryConsidering(bmi);
+
+			Console.WriteLine(category + " - " + subCategory);
+		}
+
+		private static void EjercicioBola8Magica() {
             string pregunta;
             Console.WriteLine("Ingrese la pregunta y el universo dará su respuesta (ingrese 0 para salir): ");
             pregunta = Console.ReadLine();
 
-            while (pregunta != "0")
-            {
+            while (pregunta != "0") {
                 string respuesta = Bola8Magica.Preguntar(pregunta);
                 Console.WriteLine(respuesta);
                 Console.WriteLine("Ingrese la pregunta y el universo dará su respuesta (ingrese 0 para salir): ");
@@ -26,8 +45,7 @@ namespace Clase_1_2021_1c.Consola
             }
         }
 
-        private static void EjercicioCalculadora()
-        {
+        private static void EjercicioCalculadora() {
             //comentar: ctrl + K, C
             //descomentar: ctrl + K, U
             Console.WriteLine("Ingrese un nro:");
---
+++ PW3-2021-2c-Clase-1.Logica/BodyMassIndex.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW3_2021_2c_Clase_1.Logica {
+    public static class BodyMassIndex {
+		public static double askForHeight() {
+			string heightInput;
+			double height = -1;
+
+			Console.WriteLine("Ingrese su altura en m (puede usar coma)");
+			heightInput = Console.ReadLine();
+
+			try {
+				 height = double.Parse(heightInput);
+			} catch (Exception) {
+				Console.WriteLine("El valor ingresado de altura no es válido.");
+			}
+
+			return height;
+		}
+
+		public static double askForWeight() {
+			string weightInput;
+			double weight = -1;
+
+			Console.WriteLine("Ingrese su peso en kg (puede usar coma)");
+			weightInput = Console.ReadLine();
+
+			try {
+				weight = double.Parse(weightInput);
+			} catch (Exception) {
+				Console.WriteLine("El valor ingresado de peso no es válido.");
+			}
+
+			return weight;
+		}
+
+		public static double calcBMI(double height, double weight) {
+            return weight / Math.Pow(height, 2);
+        }
+
+		public static string getCategoryConsidering(double bmi) {
+			string category = "";
+
+			if (bmi < 18.50) {
+				category = "Bajo peso";
+			} else if (bmi >= 18.50 && bmi <= 24.99) {
+				category = "Peso normal";
+			} else {
+				category = "Sobrepeso";
+			}
+
+			return category;
+		}
+
+		public static string getSubcategoryConsidering(double bmi) {
+			string subCategory = "";
+			
+			if (bmi < 18.50) {
+				subCategory = "Delgadez severa (< 18.50)";
+			} else if (bmi >= 16.00 && bmi <= 16.99) {
+				subCategory = "Delgadez moderada (16.00 - 17.00)";
+			} else if (bmi >= 17.00 && bmi <= 18.49) {
+				subCategory = "Delgadez leve (17.00 - 18.50)";
+			} else if (bmi >= 18.50 && bmi <= 24.99) {
+				subCategory = "(18.50 - 25.00)";
+			} else if (bmi >= 25.00 && bmi <= 29.99) {
+				subCategory = "Preobesidad (25.00 - 30.00)";
+			} else if (bmi >= 30.00 && bmi <= 34.99) {
+				subCategory = "Obesidad leve (30.00 - 35.00)";
+			} else if (bmi >= 35.00 && bmi <= 39.99) {
+				subCategory = "Obesidad media (35.00 - 40.00)";
+			} else if (bmi >= 40.00) {
+				subCategory = "Obesidad mórbida (> 40.00)";
+			}
+
+			return subCategory;
+		}
+
+		public static void tryAgain() {
+			Console.WriteLine("¿Reintentar? Y / N");
+			string answer = Console.ReadLine().ToUpper();
+			if (answer == "Y") {
+				Console.WriteLine("Hasta la proxima.");
+			} else if (answer == "N") {
+				Environment.Exit(0);
+			}
+		}
+	}
+}
